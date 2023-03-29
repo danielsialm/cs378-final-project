c8ac7b9e3d5b3da5d5b7e1fa9a64c0ec5240bc0f
kyzer <kyzer.polzin@gmail.com>
2023-03-29 17:12:29 -0500

    temp

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,19 +1,40 @@
 import React from "react";
 import '../App.css';
+import Home from './Home.js'
 import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom'; // version 5.2.0
+import { useNavigate } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Menu = () => {
 
+
+  const navigate = useNavigate();
   return (
     <div>
-      <div class = "menuHeader">
-        <Button>
-              	Back
-        </Button>
-      </div>
-      <p class="title-txt">
-        Your Menu
-      </p>
+      <Row>
+        <div class = "menuHeader">
+          <img class="menuBack" src={require("../assets/chevron-left.png")} onClick={() => navigate('/home')}/>
+          <img class="menuFavorite" src={require("../assets/bookmark.png")} onClick={() => navigate('/home')}/>
+        </div>
+      </Row>
+
+      <Row>
+        <Row>
+          <div class="menuTitle">
+            <h1>Your Menu</h1>
+          </div>
+          <div class="menuPrep">
+            <p>30 minute prep</p>
+          </div>
+          <div class="menuCook">
+            <p>1 hour cook</p>
+          </div>
+          <button class="menuGenerate">
+            Generate Recipe
+          </button>
+        </Row>
+      </Row>
     </div>
   )
 }
