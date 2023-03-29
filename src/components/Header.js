351f682e388f70babcc7a4c928636017a5967aaf
Donald Thai <donaldfromthailand73@gmail.com>
2023-03-29 17:17:32 -0500

    Added page navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Header.css"
+import { useNavigate } from "react-router";
+
 
 
 const Header = () => {
-
+  const navigate = useNavigate()
   return (
     <div className='header-wrapper'>
-      <img src={require("../assets/chevron-left.png")} />
+      <img src={require("../assets/chevron-left.png")} onClick={() => navigate(-1)}/>
     </div>
   )
 }
--- src/pages/home/LinkCard.js
+++ src/pages/home/LinkCard.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
-function LinkCard({Icon, text, pr}) {
+function LinkCard({Icon, text, pr, link}) {
+  const navigate = useNavigate();
+
   return (
-    <div className={`md:w-1/4 w-1/2 pb-2 ${pr ? "pr-2" : ""}`}>
+    <div className={`md:w-1/4 w-1/2 pb-2 ${pr ? "pr-2" : ""}`} onClick={() => {navigate(link)}}>
         <div className=' border-2 border-gray-200 flex flex-row items-center md:p-4 p-2 rounded-lg'>
             <Icon className = 'w-10 h-10 mr-4'/>
             <div className='text-xl font-bold text-gray-800'>{text}</div>
