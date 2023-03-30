becb1c2dfbfe06f206c7fffbf2b0f4ab74da9d41
Daniel Sialm <daniel@Daniels-MacBook-Pro.local>
2023-03-29 21:32:02 -0500

    integrated json/api into recipe list

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -3,7 +3,7 @@ import "./RecipeCard.css"
 import plus from "../assets/plus.svg"
 import check from "../assets/check.svg"
 
-const RecipeCard = ({item_name, time, img_src}) => {
+const RecipeCard = ({title, id, image}) => {
 
   const [clicked, setClicked] = useState(false)
 
@@ -13,13 +13,10 @@ const RecipeCard = ({item_name, time, img_src}) => {
 
   return (
     <div className='recipe-card-wrapper'>
-      <img src={require(`../assets/${img_src}`)} className="recipe-card-img"/>
+      <img src={image} alt={title} className="recipe-card-img"/>
       <div className='recipe-card-info-wrapper'>
         <div className='recipe-card-info'>
-          <h3>{item_name}</h3>
-          <div>
-            <p>{time} mins.</p>
-          </div>
+          <h3>{title}</h3>
         </div>
         <div onClick={handleClick} className='recipe-card-add'>
           <img src={clicked ? check : plus}/>
--- src/pages/RecipeBrowse.js
+++ src/pages/RecipeBrowse.js
@@ -1,18 +1,52 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import RecipeCard from "../components/RecipeCard";
 import "./RecipeBrowse.css"
 
+import popularRecipes from "../data/popular.json"
+
 
 const RecipeBrowse = ({page_name}) => {
-
   const [open, setOpen] = useState(false)
   const handleModalOpen = () => {
     setOpen(!open)
   }
 
+  const [recipes, setRecipes] = useState(null);
+  
+  // const src = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=09a121a6694d4d7e8473e2226fefe82f&sort=popularity'
+  // useEffect(() => {
+  //   fetch(src)
+  //     .then(response => response.json())
+  //     .then((data) => {
+  //       setRecipes(data["results"]);
+  //     })
+  //     .catch(error => console.log(error));
+  // }, []);
+
+  // local
+  useEffect(() => {
+    setRecipes(popularRecipes["results"]);
+  }, []);
+
+  const RecipesList = () => {
+    if (recipes)
+      return (
+        recipes.map(item =>
+        <div key={item["id"]}>{
+          <RecipeCard 
+            title={item["title"]}
+            id={item["id"]}
+            image={item["image"]}
+          />
+        }</div>
+        )
+      )
+    return null;
+  }
+
   return (
-    <>
+    <div>
       <div className="recipe-browse-wrapper">
         <Header />
         <div className="recipe-browse-title-wrapper">
@@ -21,27 +55,11 @@ const RecipeBrowse = ({page_name}) => {
             <img src={require("../assets/filter.png")}/>
           </div>
         </div>
-        <div>
-          <RecipeCard 
-            item_name="Pizza"
-            time="30"
-            img_src="pizza.jpg"
-          />
-
-          <RecipeCard 
-            item_name="Pan-Seared Steak"
-            time="40"
-            img_src="steak.jpg"
-          />
-
-          <RecipeCard 
-            item_name="Baked Mac and Cheese"
-            time="25"
-            img_src="m&c.jpg"
-          />
-        </div>
       </div>
-    </>
+      <div>
+        <RecipesList />
+      </div>
+    </div>
   )
 }
 
