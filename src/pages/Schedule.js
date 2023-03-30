d09c7be6b597267ae9e18c0fde69e6bc40f14ba4
kyzer <kyzer.polzin@gmail.com>
2023-03-30 10:41:44 -0500

    a VERY VERY naive schedule page

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,13 +1,38 @@
 import React from "react";
 import Header from '../components/Header';
+import { useNavigate } from "react-router";
+
 
 const Schedule = () => {
+  const navigate = useNavigate()
+
   return (
     <div>
       <Header back='/menu'/>
-      <p className="title-txt">
-        Schedule
-      </p>
+      <div>
+        <h1>Your Cooking Schedule</h1>
+      </div>
+      <div>
+        <h2>Ingredients and Equipment:</h2>
+        <ol>
+          <li>Olive Oil</li>
+          <li>10 oz. Ribeye Steak</li>
+          <li>Salt</li>
+          <li>Pan</li>
+          <li>Stove</li>
+        </ol>
+      </div>
+      <div>
+        <ol>
+          <li>Heat pan on stove</li>
+          <li>Put oil in pan while heating</li>
+          <li>Salt steak</li>
+          <li>Cook for 3 minutes</li>
+          <li>Flip and cook for an additional 3 minutes</li>
+          <li>Remove from heat. Let rest for 10 minutes</li>
+        </ol>
+      </div>
+      <button onClick= {() => {navigate('/')}}>Click here to finish</button>
     </div>
   )
 }
