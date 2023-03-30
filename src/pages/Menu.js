04335dad11514c7f535b313f550a001fddd69f19
Daniel Sialm <daniel@Daniels-MacBook-Pro.local>
2023-03-30 10:06:41 -0500

    added menuList to Your Menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import '../App.css';
 import './MenuBrowse.css';
+import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import MenuList from '../components/MenuList';
@@ -10,6 +10,22 @@ import MenuCard from '../components/MenuCard';
 const Menu = () => {
   const navigate = useNavigate();
 
+  const [recipes, setRecipes] = useState(null);
+
+  const test_data =   [
+  {
+    "id": 715424,
+    "title": "The Best Chili",
+    "image": "https://spoonacular.com/recipeImages/715424-312x231.jpg",
+    "imageType": "jpg"
+  },
+  {
+    "id": 715560,
+    "title": "World's Greatest Lasagna Roll Ups",
+    "image": "https://spoonacular.com/recipeImages/715560-312x231.jpg",
+    "imageType": "jpg"
+  }]
+
   return (
     <div>
       <div className="your-menu-wrapper">
@@ -17,12 +33,7 @@ const Menu = () => {
         <div className="your-menu-title-wrapper">
           <h1>Your Menu</h1>
         </div>
-        {/* <MenuList recipes={ recipes }/> */}
-        <MenuCard 
-          title="Pan-Seared Steak"
-          id="40"
-          image="https://spoonacular.com/recipeImages/715449-312x231.jpg"
-        />
+        <MenuList recipes={ test_data }/>
         <button onClick={() => {navigate("/schedule")}} className="menuGenerate">
           Generate Recipe
         </button>
