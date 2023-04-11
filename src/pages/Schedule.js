10ce0d96f3c3e546fbc87a65e18bb94ef504a0d6
Donald Thai <donaldfromthailand73@gmail.com>
2023-04-11 16:33:53 -0500

    Fixed header on certain pages

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -24,60 +24,62 @@ const Schedule = () => {
   const handleFinish = () => {
     window.localStorage.setItem("items", JSON.stringify([]));
     navigate("/home");
-  }
+  };
 
   return (
-    <div className="space-y-4 mx-8">
+    <>
       <Header back="/menu" />
-      <div>
-        <h1 className="text-4xl font-bold mb-8">Your Cooking Schedule</h1>
-      </div>
-      <div>
-        <h2 className="text-2xl font-bold text-gray-700 mb-4">
-          Ingredients and Equipment:
-        </h2>
-        <div className="flex flex-col w-full ">
-          {Ingredients &&
-            Ingredients.map((ingredient, i) => {
+      <div className="space-y-4 mx-8">
+        <div>
+          <h1 className="text-4xl font-bold mb-8">Your Cooking Schedule</h1>
+        </div>
+        <div>
+          <h2 className="text-2xl font-bold text-gray-700 mb-4">
+            Ingredients and Equipment:
+          </h2>
+          <div className="flex flex-col w-full ">
+            {Ingredients &&
+              Ingredients.map((ingredient, i) => {
+                return (
+                  <div className="w-full flex flex-row text-lg">
+                    <div className="mr-2">{ingredient.num}</div>
+                    <div>{ingredient.name}</div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col">
+          <h2 className="text-2xl font-bold text-gray-700 mb-4">Steps:</h2>
+          {steps &&
+            steps.map((step, i) => {
               return (
-                <div className="w-full flex flex-row text-lg">
-                  <div className="mr-2">{ingredient.num}</div>
-                  <div>{ingredient.name}</div>
+                <div className="flex flex-row w-full">
+                  <div
+                    className={`bg-blue-400 w-4 h-auto mr-4 flex flex-row items-center 
+              ${i === 0 ? "rounded-t-full" : ""} ${
+                      i === steps.length - 1 ? "rounded-b-full" : ""
+                    }
+            `}>
+                    <div className="w-4 h-4 bg-gray-100 rounded-full"></div>
+                  </div>
+                  <div className="text-2xl font-bold text-gray-700 py-2 ">
+                    {step}
+                  </div>
                 </div>
               );
             })}
         </div>
+        <Button
+          onClick={handleFinish}
+          className="schedule-finish-button"
+          fullWidth
+          variant="contained">
+          Click here to finish
+        </Button>
       </div>
-
-      <div className="w-full flex flex-col">
-        <h2 className="text-2xl font-bold text-gray-700 mb-4">Steps:</h2>
-        {steps &&
-          steps.map((step, i) => {
-            return (
-              <div className="flex flex-row w-full">
-                <div
-                  className={`bg-blue-400 w-4 h-auto mr-4 flex flex-row items-center 
-              ${i === 0 ? "rounded-t-full" : ""} ${
-                    i === steps.length - 1 ? "rounded-b-full" : ""
-                  }
-            `}>
-                  <div className="w-4 h-4 bg-gray-100 rounded-full"></div>
-                </div>
-                <div className="text-2xl font-bold text-gray-700 py-2 ">
-                  {step}
-                </div>
-              </div>
-            );
-          })}
-      </div>
-      <Button
-        onClick={handleFinish}
-        className="schedule-finish-button"
-        fullWidth
-        variant="contained">
-        Click here to finish
-      </Button>
-    </div>
+    </>
   );
 };
 
