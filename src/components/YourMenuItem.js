424e2b736c068cb9748aeaca3ccd075f3aeb77e3
Donald Thai <donaldfromthailand73@gmail.com>
2023-04-14 00:11:24 -0500

    Fixed clicking on delete navigates to page and header on recipe page

--- src/components/YourMenuItem.js
+++ src/components/YourMenuItem.js
@@ -7,7 +7,7 @@ const YourMenuItem = ({id, title, reload, setReload}) => {
   const navigate = useNavigate();
 
 
-  const handleDelete = () => {
+  const handleDelete = (e) => {
     let items = JSON.parse(window.localStorage.getItem("items"))
     let index = items.findIndex((item) => item.id === id);
     
@@ -19,6 +19,7 @@ const YourMenuItem = ({id, title, reload, setReload}) => {
       window.localStorage.setItem("items", JSON.stringify(newArray));
       setReload(reload+1)
     }
+    e.stopPropagation()
   }
 
   return (
@@ -26,8 +27,8 @@ const YourMenuItem = ({id, title, reload, setReload}) => {
       className="w-full py-4 border-gray-100 shadow border-2 mb-4 rounded-lg px-2 flex justify-between items-center"
       onClick={() => navigate("/recipe/" + id)}
     >
-      <h4 className="text-lg">{title}</h4>
-      <X className="stroke-red-500" onClick={handleDelete} />
+      <h4 className="text-lg flex-wrap w-11/12">{title}</h4>
+      <X className="stroke-red-500 w-1/12" onClick={handleDelete} />
     </div>
   );
 };
--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,15 +1,14 @@
 import React from "react";
-import '../App.css';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import "../App.css";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { Button } from "@mui/material";
-import Header from '../components/Header';
+import Header from "../components/Header";
 import { firebaseConfig, auth } from "../pages/firebase";
 import axios from "axios";
 import { ReactComponent as Bookmark } from "../assets/bookmark.svg";
 
-
-import recipeData from '../data/recipe.json';
+import recipeData from "../data/recipe.json";
 
 const Recipe = () => {
   const { id } = useParams();
@@ -22,7 +21,6 @@ const Recipe = () => {
   const [steps, setSteps] = useState(["Loading..."]);
   const [save, setSaved] = useState(false);
   const [savedRecipes, setSavedRecipes] = useState([]);
-
 
   let currentItem = {};
   currentItem.title = name;
@@ -41,16 +39,14 @@ const Recipe = () => {
       alert("Added recipe to your menu!");
       newArray = [...items, currentItem];
     } else {
-      alert("Recipe is already part of your menu!")
+      alert("Recipe is already part of your menu!");
     }
 
     console.log(newArray);
     window.localStorage.setItem("items", JSON.stringify(newArray));
-  }
-
+  };
 
   const handleSave = (e) => {
-
     if (!auth || !auth.currentUser) {
       e.stopPropagation();
       alert("Please login to save recipes!");
@@ -60,7 +56,7 @@ const Recipe = () => {
         return recipe.id === id;
       });
 
-      console.log(index)
+      console.log(index);
 
       // Recipe not saved yet
       if (index === -1) {
@@ -68,46 +64,49 @@ const Recipe = () => {
         console.log(currentItem);
         axios
           .post(
-            `${firebaseConfig.databaseURL + "/" + auth.currentUser.uid
+            `${
+              firebaseConfig.databaseURL + "/" + auth.currentUser.uid
             }/recipes.json`,
             JSON.stringify(currentItem)
           )
           .then((res) => {
             setSavedRecipes([...savedRecipes, currentItem]);
-            setSaved(true)
+            setSaved(true);
           });
       } else {
-
-        // Need to get from database because need the db assigned id of the 
+        // Need to get from database because need the db assigned id of the
         // items so we can delete the right one
         axios
           .get(
-            `${firebaseConfig.databaseURL + "/" + auth.currentUser.uid
+            `${
+              firebaseConfig.databaseURL + "/" + auth.currentUser.uid
             }/recipes.json`,
             {
               method: "GET",
             }
-          ).then((res) => {
-
+          )
+          .then((res) => {
             // Get the id of item in database and use it to delete the item
-            let db_item_id = Object.keys(res.data)[index]
-            setSaved(false)
-            axios.delete(`${firebaseConfig.databaseURL + "/" + auth.currentUser.uid
-              }/recipes/${db_item_id}.json`)
+            let db_item_id = Object.keys(res.data)[index];
+            setSaved(false);
+            axios
+              .delete(
+                `${
+                  firebaseConfig.databaseURL + "/" + auth.currentUser.uid
+                }/recipes/${db_item_id}.json`
+              )
               .then((res) => {
-
                 // update our state
-                let copy = [...savedRecipes]
-                copy.splice(index, 1)
-                setSavedRecipes(copy)
-              })
-          })
+                let copy = [...savedRecipes];
+                copy.splice(index, 1);
+                setSavedRecipes(copy);
+              });
+          });
       }
     }
 
     e.stopPropagation();
   };
-
 
   // src = 'http://api.spoonacular.com/recipes/{id}/information?apiKey=09a121a6694d4d7e8473e2226fefe82f';
   // useEffect(() => {
@@ -126,21 +125,25 @@ const Recipe = () => {
     setTime(recipeData["readyInMinutes"]);
     setImage(recipeData["image"]);
 
-
-    setIngredients(recipeData["extendedIngredients"].map(item => item.original));
-    setSteps(recipeData["analyzedInstructions"][0]["steps"].map(item => item.step));
+    setIngredients(
+      recipeData["extendedIngredients"].map((item) => item.original)
+    );
+    setSteps(
+      recipeData["analyzedInstructions"][0]["steps"].map((item) => item.step)
+    );
     const equipmentSet = new Set();
-    recipeData["analyzedInstructions"][0]["steps"].forEach(element => {
-      element.equipment.forEach(item => equipmentSet.add(item.name));
+    recipeData["analyzedInstructions"][0]["steps"].forEach((element) => {
+      element.equipment.forEach((item) => equipmentSet.add(item.name));
     });
     setEquipment(Array.from(equipmentSet));
 
     //saving the recipe
     if (auth && auth.currentUser) {
-      console.log("running")
+      console.log("running");
       axios
         .get(
-          `${firebaseConfig.databaseURL + "/" + auth.currentUser.uid
+          `${
+            firebaseConfig.databaseURL + "/" + auth.currentUser.uid
           }/recipes.json`,
           {
             method: "GET",
@@ -153,74 +156,80 @@ const Recipe = () => {
 
             // this useEffect doesn't run when state is altered. So to avoid
             // having to make an api call each time, we will save the recipes
-            // Probably don't need to save recipes. Can just do a get request 
+            // Probably don't need to save recipes. Can just do a get request
             // each time you click on save. In fact, that's probably better tbh
             setSaved(recipes.some((recipe) => recipe.id === id));
             setSavedRecipes(recipes);
           }
         });
     }
-
   }, []);
 
   return (
-
-    <div className="space-y-4 mx-8">
-      <Header back={-1} Right={Bookmark} rightOnClick={handleSave} rightStyle={`w-8 h-8 ${save ? "fill-[#FEE135]" : ""
-        } stroke-[#FEE135]`} />
-      <div>
-        <h1 className="text-4xl font-bold mb-8">{name}</h1>
-      </div>
-      <div className="flex flex-col w-half items-center">
-        <img className="p-4" src={image} alt={name} />
-
-      </div>
-      <div>
-        <h2 className="text-2xl font-bold text-gray-700 mb-4">
-          Ingredients:
-        </h2>
-        <div className="flex flex-col w-full text-base text-gray-700 p-0.5 ps-4" >
-          {ingredients.map(item => <li className="text-base text-gray-700" key={item}>{item}</li>)}
+    <>
+      <Header
+        back={-1}
+        Right={Bookmark}
+        rightOnClick={handleSave}
+        rightStyle={`w-8 h-8 ${save ? "fill-[#FEE135]" : ""} stroke-[#FEE135]`}
+      />
+      <div className="space-y-4 mx-8">
+        <div>
+          <h1 className="text-4xl font-bold mb-8">{name}</h1>
         </div>
-        <h2 className="text-2xl font-bold text-gray-700 my-4">
-          Equipment:
-        </h2>
-        <div className="flex flex-col w-full text-base text-gray-700 p-0.5 ps-4" >
-          {equipment.map(item => <li className="text-base text-gray-700" key={item}>{item}</li>)}
+        <div className="flex flex-col w-half items-center">
+          <img className="p-4" src={image} alt={name} />
         </div>
-      </div>
+        <div>
+          <h2 className="text-2xl font-bold text-gray-700 mb-4">
+            Ingredients:
+          </h2>
+          <div className="flex flex-col w-full text-base text-gray-700 p-0.5 ps-4">
+            {ingredients.map((item) => (
+              <li className="text-base text-gray-700" key={item}>
+                {item}
+              </li>
+            ))}
+          </div>
+          <h2 className="text-2xl font-bold text-gray-700 my-4">Equipment:</h2>
+          <div className="flex flex-col w-full text-base text-gray-700 p-0.5 ps-4">
+            {equipment.map((item) => (
+              <li className="text-base text-gray-700" key={item}>
+                {item}
+              </li>
+            ))}
+          </div>
+        </div>
 
-      <div className="w-full flex flex-col">
-        <h2 className="text-2xl font-bold text-gray-700 mb-4">Steps:</h2>
-        {steps &&
-          steps.map((step, i) => {
-            return (
-              <div className="flex flex-row w-full">
-                <div
-                  className={`bg-blue-400 w-4 h-auto mr-4 flex flex-row items-center 
-            ${i === 0 ? "rounded-t-full" : ""} ${i === steps.length - 1 ? "rounded-b-full" : ""
+        <div className="w-full flex flex-col">
+          <h2 className="text-2xl font-bold text-gray-700 mb-4">Steps:</h2>
+          {steps &&
+            steps.map((step, i) => {
+              return (
+                <div className="flex flex-row w-full">
+                  <div
+                    className={`bg-blue-400 w-4 h-auto mr-4 flex flex-row items-center 
+            ${i === 0 ? "rounded-t-full" : ""} ${
+                      i === steps.length - 1 ? "rounded-b-full" : ""
                     }
           `}>
-                  <div className="w-4 h-4 bg-gray-100 rounded-full"></div>
+                    <div className="w-4 h-4 bg-gray-100 rounded-full"></div>
+                  </div>
+                  <div className="text-lg text-gray-700 py-2 ">{step}</div>
                 </div>
-                <div className="text-lg text-gray-700 py-2 ">
-                  {step}
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
+        <Button
+          onClick={handleFinish}
+          className="schedule-finish-button"
+          fullWidth
+          variant="contained">
+          Add to menu
+        </Button>
       </div>
-      <Button
-        onClick={handleFinish}
-        className="schedule-finish-button"
-        fullWidth
-        variant="contained">
-        Add to menu
-      </Button>
-    </div>
+    </>
   );
-
-
-}
+};
 
 export default Recipe;
